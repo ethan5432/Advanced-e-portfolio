a5f04e03d9621b6d19ebc42725727dfc1c8571b7
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-11 09:30:50 -0400

    javascript good, need to figure out visibility

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ function contact(event) {
     })
 }
 
-// let isModalOpen = false;
+let isModalOpen = false;
 function toggleModal() {
     console.log('toggleModal()')
-//     if (isModalOpen) {
-//         return document.body.classList.remove("modal--open")
-//     }
+    if (isModalOpen) {
+        return document.body.classList.remove("modal--open")
+    }
    
-//       isModalOpen = !isModalOpen;
-//     document.body.classList += " modal--open";
+      isModalOpen = !isModalOpen;
+    document.body.classList += " modal--open";
 }
